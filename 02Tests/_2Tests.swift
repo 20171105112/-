99938ef06016cd145385b6bf71f1daf93b220086
20171105112 <903056683@qq.com>
2018-09-28 08:27:50 +0800

    Initial Commit

---
+++ 02Tests/_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _2Tests.swift
+//  02Tests
+//
+//  Created by 黄宏 on 2018/9/28.
+//  Copyright © 2018年 黄宏. All rights reserved.
+//
+
+import XCTest
+@testable import _2
+
+class _2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
