97ed0125021b6cf84b77b95e6878ab49019e1cef
20171105112 <903056683@qq.com>
2018-10-26 10:01:15 +0800

    可以实现 连续加法

--- 02/ViewController.swift
+++ 02/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     var temp:Double = 0
-    var x:Double = 0
+    var x = 0
+    var y = 0
     var sum1:Double = 0
     var sum:Double = 0
     @IBOutlet weak var text: UITextField!
@@ -21,35 +22,124 @@ class ViewController: UIViewController {
     }
     @IBAction func button1(_ sender: Any)
     {
-        text.text = text.text! + "1"
+        if y == 1
+        {
+            text.text = text.text! + "1"
+        }
+        else
+        {
+            text.text = "1"
+        }
+        y = 1
+        
     }
     @IBAction func button2(_ sender: Any)
     {
-        text.text = text.text! + "2"
+        if y == 1
+        {
+            text.text = text.text! + "2"
+        }
+        else
+        {
+            text.text = "2"
+        }
+        y = 1
     }
     @IBAction func button3(_ sender: Any)
     {
-        text.text = text.text! + "3"
+        if y == 1
+        {
+            text.text = text.text! + "3"
+        }
+        else
+        {
+            text.text = "3"
+        }
+        y = 1
     }
-    @IBAction func button4(_ sender: Any) {text.text = text.text! + "4"
+    @IBAction func button4(_ sender: Any)
+    {
+        if y == 1
+    {
+        text.text = text.text! + "4"
+    }
+    else
+    {
+        text.text = "4"
+    }
+        y = 1
     }
     @IBAction func button5(_ sender: Any)
-    {text.text = text.text! + "5"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "5"
+        }
+        else
+        {
+            text.text = "5"
+        }
+        y = 1
     }
     @IBAction func button6(_ sender: Any)
-    {text.text = text.text! + "6"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "6"
+        }
+        else
+        {
+            text.text = "6"
+        }
+        y = 1
     }
     @IBAction func button7(_ sender: Any)
-    {text.text = text.text! + "7"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "7"
+        }
+        else
+        {
+            text.text = "7"
+        }
+        y = 1
     }
     @IBAction func button8(_ sender: Any)
-    {text.text = text.text! + "8"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "8"
+        }
+        else
+        {
+            text.text = "8"
+        }
+        y = 1
     }
     @IBAction func button9(_ sender: Any)
-    {text.text = text.text! + "9"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "9"
+        }
+        else
+        {
+            text.text = "9"
+        }
+        y = 1
     }
     @IBAction func button0(_ sender: Any)
-    {text.text = text.text! + "0"
+    {
+        if y == 1
+        {
+            text.text = text.text! + "0"
+        }
+        else
+        {
+            text.text = "0"
+        }
+        y = 1
     }
     
     @IBAction func buttonPi(_ sender: Any)
@@ -81,7 +171,7 @@ class ViewController: UIViewController {
     
     @IBAction func square(_ sender: Any) {
         x = 1
-        temp = Double                                                                                                                                                                                                                      (text.text!)!
+        temp = Double                                                                                                                                                                                                                                                   (text.text!)!
         text.text = ""
     }
     
@@ -99,7 +189,6 @@ class ViewController: UIViewController {
         if (x == -1)
         {
             sum1 = temp + Double(text.text!)!
-           
             print("sum1=\(sum1) temp=\(temp)")
         }
         if (x == -2)
@@ -132,9 +221,13 @@ class ViewController: UIViewController {
     }
     @IBAction func buttonadd(_ sender: Any)
     {
-        x = -1
+        if x == -1{
+            text.text = "\(temp + Double(text.text!)!)"
+            
+        }
         temp = Double(text.text!)!
-        text.text = ""
+        y = 2
+        x = -1
     }
     @IBAction func buttonminu(_ sender: Any)
     {
